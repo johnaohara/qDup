92a0626116fa3e60d6c09b18dc0cfb5351c156d3
John OHara <johara@redhat.com>
2021-04-08 13:42:12 +0100

    Jbnag script tracking current master branch

--- docs/examples/runQDup.java
+++ docs/examples/runQDup.java
@@ -1,5 +1,5 @@
 //usr/bin/env jbang "$0" "$@" ; exit $?
-//DEPS io.hyperfoil.tools:qDup:0.6.2
+//DEPS io.hyperfoil.tools:qDup:0.6.3-SNAPSHOT
 //DEPS org.apache.commons:commons-lang3:3.12.0
 
 import org.apache.commons.lang3.ArrayUtils;
